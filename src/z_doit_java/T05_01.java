d7c7950ea431a4c59bf5bf634ee8de410fe262ec
MiraeChoi <pnenv@naver.com>
2022-04-17 23:51:56 +0900

    add a new class

---
+++ src/z_doit_java/T05_01.java
@@ -0,0 +1,21 @@
+package z_doit_java;
+
+import java.util.Scanner;
+
+class T05_01 {
+	static int factorial(int n) {
+		int answer = 1;
+		if(n <= 2) return n;
+		else {
+			for(int i = n; i >= 1; i--) answer *= i;
+			return answer;
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner kb = new Scanner(System.in);
+		System.out.print("정수를 입력하세요. : ");
+		int n = kb.nextInt();
+		System.out.println(n + "의 팩토리얼은 " + factorial(n) + "입니다.");
+	}
+}
