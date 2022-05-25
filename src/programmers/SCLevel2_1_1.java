256c61796ead1c7f160a487ff3bef6a630e303f7
MiraeChoi <pnenv@naver.com>
2022-05-25 19:48:46 +0900

    add test 1 and 2

--- src/programmers/SCLevel2_1_1.java
+++ src/programmers/SCLevel2_1_1.java
@@ -1,5 +1,41 @@
 package programmers;
 
+import java.util.Arrays;
+
 class SCLevel2_1_1 {
+	//구명보트를 최대한 적게 사용하여 모든 사람 구출하기
+	//구명보트는 한 번에 최대 2명까지 탈 수 있다.
+	//모든 사람을 구출하기 위해 필요한 구명보트 개수의 최솟값을 return한다.
 	
+	//무인도에 갇힌 사람 : 1명 이상 50,000명 이하
+	//각 사람의 몸무게 : 40kg 이상 240kg 이하
+	//구명보트의 무게 제한 : 40kg 이상 240kg 이하
+	//구명보트의 무게 제한은 항상 사람들의 몸무게 중 최댓값보다 크게 주어진다.
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        Arrays.sort(people);
+        int cnt = 0;
+        
+        for(int i = 0; i < people.length; i++) {
+        	cnt += people[i];
+        	if(cnt >= 100) {
+        		answer++;
+        		cnt = 0;
+        	}
+        	System.out.println(i + " : " + answer + " / " + cnt);
+        	if(i == people.length - 1) answer++;
+        }
+        return answer;
+    }
+    
+    public static void main(String[] args) {
+		SCLevel2_1_1 T = new SCLevel2_1_1();
+		//return 3
+		int[] people = {70, 50, 80, 50};
+		int limit = 100;
+		//return 3
+//		int[] people = {70, 80, 50};
+//		int limit = 100;
+		System.out.println(T.solution(people, limit));
+	}
 }
--- src/programmers/SCLevel2_1_2.java
+++ src/programmers/SCLevel2_1_2.java
@@ -1,5 +1,48 @@
 package programmers;
 
 class SCLevel2_1_2 {
+	//트리 형태로 연결된 n개의 송전탑 전선 중 하나를 끊어 전력망 네트워크를 2개로 분할하려 한다.
+	//이때 두 전력망이 갖게 되는 송전탑의 개수를 최대한 비슷하게 맞추어 그 절대값을 return한다.
 	
+	//n : 2 이상 100 이하
+	//wires : 길이가 n-1인 정수형 2차원 배열. {v1, v2} 2개의 자연수로 이루어져 있다.
+	//1 <= v1 <= v2 <= n
+	//전력망 네트워크가 하나의 트리 형태가 아닌 경우 입력으로 주어지지 않는다.
+    public int solution(int n, int[][] wires) {
+        int answer = -1;
+        return answer;
+    }
+    
+    public static void main(String[] args) {
+		SCLevel2_1_2 T = new SCLevel2_1_2();
+		//return 3
+		int n = 9;
+		int[][] wires = new int[n-1][2];
+		wires[0][0] = 1;	wires[0][1] = 3;	//1 3
+		wires[1][0] = 2;	wires[1][1] = 3;	//2 3
+		wires[2][0] = 3;	wires[2][1] = 4;	//3 4
+		wires[3][0] = 4;	wires[3][1] = 5;	//4 5
+		wires[4][0] = 4;	wires[4][1] = 6;	//4 6
+		wires[5][0] = 4;	wires[5][1] = 7;	//4 7
+		wires[6][0] = 7;	wires[6][1] = 8;	//7 8
+		wires[7][0] = 7;	wires[7][1] = 9;	//7 9
+		
+		//return 0
+//		int n = 4;
+//		int[][] wires = new int[n-1][2];
+//		wires[0][0] = 1;	wires[0][1] = 2;	//1 2
+//		wires[1][0] = 2;	wires[1][1] = 3;	//2 3
+//		wires[2][0] = 3;	wires[2][1] = 4;	//3 4
+		
+		//return 1
+//		int n = 7;
+//		int[][] wires = new int[n-1][2];
+//		wires[0][0] = 1;	wires[0][1] = 2;	//1 2
+//		wires[1][0] = 2;	wires[1][1] = 7;	//2 7
+//		wires[2][0] = 3;	wires[2][1] = 7;	//3 7
+//		wires[3][0] = 3;	wires[3][1] = 4;	//3 4
+//		wires[4][0] = 4;	wires[4][1] = 5;	//4 5
+//		wires[5][0] = 6;	wires[5][1] = 7;	//6 7
+		System.out.println(T.solution(n, wires));
+    }
 }
