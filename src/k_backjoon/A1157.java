07645bc94a7a03200650f511383111f812f023a9
MiraeChoi <pnenv@naver.com>
2022-05-30 23:59:23 +0900

    add a new class

---
+++ src/k_backjoon/A1157.java
@@ -0,0 +1,16 @@
+package k_backjoon;
+
+import java.util.Scanner;
+
+class A1157 {
+	static String solution(String word) {
+		
+		return "";
+	}
+	
+	public static void main(String[] args) {
+		Scanner kb = new Scanner(System.in);
+		String word = kb.nextLine();
+		System.out.println(solution(word));
+	}
+}
