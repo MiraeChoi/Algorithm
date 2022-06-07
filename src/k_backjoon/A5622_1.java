6bb6f615817de4cc2458d3f6e0a42ee41d01bd0e
MiraeChoi <pnenv@naver.com>
2022-06-08 00:32:40 +0900

    add A5622

--- src/k_backjoon/A5622_1.java
+++ src/k_backjoon/A5622_1.java
@@ -1,5 +1,24 @@
 package k_backjoon;
 
+import java.util.Scanner;
+
 class A5622_1 {
-	
+	public static void main(String[] args) {
+		Scanner kb = new Scanner(System.in);
+		String str = kb.next();
+		int answer = 0;
+		
+		for(int i = 0; i < str.length(); i++) {
+			char tmp = str.charAt(i);
+			if(tmp >= 'A' && tmp <= 'C') answer += 3;
+			if(tmp >= 'D' && tmp <= 'F') answer += 4;
+			if(tmp >= 'G' && tmp <= 'I') answer += 5;
+			if(tmp >= 'J' && tmp <= 'L') answer += 6;
+			if(tmp >= 'M' && tmp <= 'O') answer += 7;
+			if(tmp >= 'P' && tmp <= 'S') answer += 8;
+			if(tmp >= 'T' && tmp <= 'V') answer += 9;
+			if(tmp >= 'W' && tmp <= 'Z') answer += 10;			
+		}
+		System.out.println(answer);
+	}
 }
--- src/k_backjoon/A5622_2.java
+++ src/k_backjoon/A5622_2.java
@@ -1,5 +1,14 @@
 package k_backjoon;
 
-public class A5622_2 {
+import java.io.IOException;
 
+public class A5622_2 {
+	public static void main(String[] args) throws IOException {
+		int n, answer = 0;
+		while((n = System.in.read() - 65) > -1) {
+			System.out.println(n);
+			answer += (n - n / 18 - n / 25) / 3 + 3;
+		}
+		System.out.print(answer);
+	}
 }
