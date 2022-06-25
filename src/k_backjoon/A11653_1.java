7ef9ecf3f3b3687a93d94e4efb79acbb72da896f
MiraeChoi <pnenv@naver.com>
2022-06-25 23:41:41 +0900

    add a new class

--- src/k_backjoon/A11653_1.java
+++ src/k_backjoon/A11653_1.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class A11653 {
+public class A11653_1 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
---
+++ src/k_backjoon/A11653_2.java
@@ -0,0 +1,5 @@
+package k_backjoon;
+
+class A11653_2 {
+	
+}
