ebbc008104e82eb59b05bf559ed87590c3eec89e
MiraeChoi <pnenv@naver.com>
2022-06-27 23:58:28 +0900

    add qnas

--- src/k_backjoon/A11653_1.java
+++ src/k_backjoon/A11653_1.java
@@ -5,12 +5,26 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class A11653_1 {
+	//맞았습니다!!
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		if(n == 1) return;
-		else {
-			//
+		
+		while(n >= 2) {
+			if(n % 2 == 0) {
+				while(n % 2 == 0) {
+					System.out.println(2);
+					n /= 2;
+				}
+			}
+			for(int i = 3; i <= n; i += 2) {
+				if(n % i == 0) {
+					while(n % i == 0) {
+						System.out.println(i);
+						n /= i;
+					}
+				}
+			}
 		}
 	}
 }
--- src/k_backjoon/A11653_2.java
+++ src/k_backjoon/A11653_2.java
@@ -1,5 +1,23 @@
 package k_backjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 class A11653_2 {
-	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i = 2; i * i <= n; i++) {
+			System.out.println("i : " + i);
+			while(n % i == 0) {
+				sb.append(i + "\n");
+				n /= i;
+			}
+		}
+		if(n != 1) sb.append(n);
+		System.out.println(sb);
+	}
 }
