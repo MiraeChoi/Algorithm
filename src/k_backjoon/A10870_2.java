5d38d9629c43539d95e5e422eb462dcfdcbcbe86
MiraeChoi <pnenv@naver.com>
2022-07-10 23:59:17 +0900

    edit my answer

--- src/k_backjoon/A10870_2.java
+++ src/k_backjoon/A10870_2.java
@@ -1,5 +1,11 @@
 package k_backjoon;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 class A10870_2 {
-	
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		num = Integer.parseInt(br.readLine());
+	}
 }
