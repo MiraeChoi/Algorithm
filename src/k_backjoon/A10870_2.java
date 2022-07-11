0e28fe4029490a1dd12bad663d0530c63d56d572
MiraeChoi <pnenv@naver.com>
2022-07-11 23:59:39 +0900

    edit my answer

--- src/k_backjoon/A10870_2.java
+++ src/k_backjoon/A10870_2.java
@@ -1,11 +1,13 @@
 package k_backjoon;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 class A10870_2 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		num = Integer.parseInt(br.readLine());
+		int num = Integer.parseInt(br.readLine());
+		
 	}
 }
--- src/k_backjoon/A9020_2.java
+++ src/k_backjoon/A9020_2.java
@@ -1,5 +1,47 @@
 package k_backjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 class A9020_2 {
-	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		int max = Integer.MIN_VALUE;
+		for(int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+			max = Math.max(max, arr[i]);
+		}
+		boolean[] prime = new boolean[max + 1];
+		prime[0] = prime[1] = true;
+		for(int i = 2; i <= max; i++) {
+			if(prime[i] == false) {
+				for(int j = i * 2; j <= max; j += i) {
+					prime[j] = true;
+				}
+			}
+		}
+		for(int i = 0; i < n; i++) {
+			int idx = arr[i] / 2;
+			if(!prime[idx]) sb.append(idx + " " + idx + "\n");
+			else {
+				int n1 = idx - 1;
+				int n2 = idx + 1;
+				while(true) {
+					while(prime[n1] && n1 >= 0) n1--;
+					while(prime[n2] && n2 <= max) n2++;
+					if(n1 + n2 == arr[i]) {
+						sb.append(n1 + " " + n2 + "\n");
+						break;
+					} else {
+						n1--; n2++;
+					}
+				}
+			}
+		}
+		System.out.println(sb);
+	}
 }
