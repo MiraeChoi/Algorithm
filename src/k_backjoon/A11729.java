5d7c630d67ea6d731c85d8660f37324289837712
MiraeChoi <pnenv@naver.com>
2022-07-20 23:57:58 +0900

    add an answer

--- src/k_backjoon/A11729.java
+++ src/k_backjoon/A11729.java
@@ -1,7 +1,13 @@
 package k_backjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 class A11729 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
 		
 	}
 }
