22a299a9c6a3fbc30b987a9acf9d015f7b7d2148
MiraeChoi <pnenv@naver.com>
2022-07-22 22:27:48 +0900

    add my answer

--- src/k_backjoon/A2798.java
+++ src/k_backjoon/A2798.java
@@ -1,7 +1,23 @@
 package k_backjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class A2798 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int[] arr = new int[n];
+		for(int i = 0; i < n; i++) arr[i] = Integer.parseInt(st.nextToken());
+		int c1, c2, c3;
+		c2 = n / 2;
+		c1 = c2 - 1;
+		c3 = c2 + 1;
+		int answer = 0;
 		
 	}
 }
