7858da743c58a95b892f6cfed3e96220acf883f8
MiraeChoi <pnenv@naver.com>
2022-07-29 09:35:58 +0900

    add qnas

---
+++ src/k_backjoon/A2231_1.java
@@ -0,0 +1,40 @@
+package k_backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class A2231_1 {
+	//맞았습니다!!
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int sum = n;
+		for(int i = 1; i < n; i++) {
+			sum = i;
+			System.out.println("i : " + i);
+			String tmp = i + "";
+			for(int j = 0; j < tmp.length(); j++) {
+				sum += Integer.parseInt(tmp.substring(j, j + 1));
+				System.out.println("sum : " + sum);
+			}
+			if(sum == n) {
+				System.out.println(i);
+				return;
+			}
+		}
+		System.out.println(0);
+//		for(int i = n; i > 1; i--) {
+//			sum = i;
+//			System.out.println("i : " + i);
+//			String tmp = i + "";
+//			for(int j = 0; j < tmp.length(); j++) {
+//				sum += Integer.parseInt(tmp.substring(j, j + 1));
+//				System.out.println("sum : " + sum);
+//			}
+//			if(sum == n) answer = Math.min(answer, i);
+//		}
+//		if(answer != n) System.out.println(answer);
+//		else System.out.println(0);
+	}
+}
---
+++ src/k_backjoon/A2231_2.java
@@ -0,0 +1,7 @@
+package k_backjoon;
+
+class A2231_2 {
+	public static void main(String[] args) {
+		
+	}
+}
