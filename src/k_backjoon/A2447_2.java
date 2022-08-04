8ca75a3e95efca99a7df1fff1f79a78d784c4414
MiraeChoi <pnenv@naver.com>
2022-08-04 17:45:17 +0900

    try to understanding other's answer

--- src/k_backjoon/A2447_2.java
+++ src/k_backjoon/A2447_2.java
@@ -20,27 +20,30 @@ class A2447_2 {
  
 	static void star(int x, int y, int N, boolean blank) {	//x=0, y=0, N=9, false	x=0, y=0, N=3, false																							x=1, y=1, N=1, true																													x=3, y=3, N=3, true
 		if(blank) {
+			System.out.println("★x=" + x + ", y=" + y + ", N = " + N + ", " + blank);
 			for(int i = x; i < x + N; i++) {				//																																		i = 1; i < 2; i++																													i = 3; i < 6; i++
 				for(int j = y; j < y + N; j++) sb[i].setCharAt(j, ' ');	//																															j = 1; j < 2; j++																													j = 3; j < 6; j++
 			}												//																																		sb[1].charAt(1, ' ');																												sb[3~5].charAt(3~5, ' ');	
 			return;
 		}
-		// 더이상 쪼갤 수 없는 블록일 때
+		//더 이상 쪼갤 수 없는 블록일 때
 		if(N == 1) return;
 		
 		/*
-		   N=27 일 경우 한 블록의 사이즈는 9이고, 
-		   N=9 일 경우 한 블록의 사이즈는 3이듯
+		   N=27일 경우 한 블록의 사이즈는 9이고, 
+		   N=9일 경우 한 블록의 사이즈는 3이듯
 		   해당 블록의 한 칸을 담을 변수를 의미 size
            
 		   count는 별 출력 누적을 의미
 		 */
- 
+		System.out.println("-------------------------");
+		System.out.println("start : x=" + x + ", y=" + y + ", N = " + N);
 		int size = N / 3;									//size = 9/3 = 3		size = 3/3 = 1					size = 3/3 = 1																						size = 9/3 = 3							size = 9/3 = 3				size = 9/3 = 3				size = 9/3 = 3									size = 9/3 = 3
 		int count = 0;
 		for(int i = x; i < x + N; i += size) {				//i = 0, i < 9; i += 3	i = 0, i < 3; i += 1											i = 1, i < 3			i = 1, i < 3								i = 0, i < 9; i += 3					i = 0, i < 9; i += 3		i = 3, i < 9; i += 3		i = 3, i < 9; i += 3							i = 3, i < 9; i += 3
 			for(int j = y; j < y + N; j += size) {			//j = 0, j < 9; j += 3	j = 0, j < 3; j += 1			j = 1, j < 3	j = 2, j < 3	j = 0, j < 3; j += 1	j = 1, j < 3; j += 1						j = 3, j < 9; j += 3					j = 6, j < 9; j += 3		j = 0, j < 9; j += 3		j = 3, j < 9; j += 3							j = 6, j < 9; j += 3
 				count++;									//cnt = 1				cnt = 1							cnt = 2			cnt = 3			cnt = 4					cnt = 5										cnt = 2									cnt = 3						cnt = 4						cnt = 5											cnt = 6
+				System.out.println("i=" + i + ", j=" + j + ", N = " + N + ", count=" + count);
 				//공백 칸일 경우
 				if(count == 5) star(i, j, size, true);		//																												star(1, 1, 1, true)														↗아닐수도있으니다시해보자													star(3, 3, 3, true)
 				else star(i, j, size, false);				//star(0, 0, 3, false)	star(0, 0, 1, false) -> return	0 1 1 f > r		0 2 1 f > r		1 0 1 f > r															star(0, 3, 3, false) -> 어차피 다 return	star(0, 6, 3, false) > r	star(3, 0, 3, false) > r													star(3, 6, 3, false)
---
+++ src/k_backjoon/A2447_3.java
@@ -0,0 +1,8 @@
+package k_backjoon;
+
+class A2447_3 {
+	//직접 구현
+	public static void main(String[] args) {
+		
+	}
+}
