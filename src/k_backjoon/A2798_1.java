eeba01448debee71ee740f490331296dc9daf205
MiraeChoi <pnenv@naver.com>
2022-08-06 23:23:27 +0900

    add qnas

--- src/k_backjoon/A2798_1.java
+++ src/k_backjoon/A2798_1.java
@@ -15,36 +15,26 @@ public class A2798_1 {
 		int[] arr = new int[n];
 		st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < n; i++) arr[i] = Integer.parseInt(st.nextToken());
-		int c1 = 0, c2 = 1, c3 = 2;
-		int answer = -1;
-		while(true) {
-			int sum = arr[c1] + arr[c2] + arr[c3];
-			System.out.println(c1 + " / " + c2 + " / " + c3);
-			if(sum > m) {
-				c3++;
-				if(c3 == n) {
-					c2++;
-					c3 = c2 + 1;
-					if(c2 == n-1) {
-						c1++;
-						if(c1 == n-2) break;
-						c2 = c1 + 1;
-						c3 = c2 + 1;					
-					}
-				}
-				continue;
+		
+		int n1 = 0, n2 = 1, n3 = 2;
+		int answer = 0;
+		
+		while(n1 < n-2) {
+			int sum = arr[n1] + arr[n2] + arr[n3];
+			if(sum == m) {
+				System.out.println(m);
+				return;
+			} else if(sum < m) {
+				answer = Math.max(answer, sum);
 			}
-			if(Math.abs(m - answer) > Math.abs(m - sum)) answer = sum;
-			if(answer == m) break;
-			c3++;
-			if(c3 == n) {
-				c2++;
-				c3 = c2 + 1;
-				if(c2 == n-1) {
-					c1++;
-					if(c1 == n-2) break;
-					c2 = c1 + 1;
-					c3 = c2 + 1;					
+			n3++;
+			if(n3 >= n) {
+				n2++;
+				n3 = n2 + 1;
+				if(n2 >= n-1) {
+					n1++;
+					n2 = n1 + 1;
+					n3 = n2 + 1;
 				}
 			}
 		}
--- src/k_backjoon/A2798_2.java
+++ src/k_backjoon/A2798_2.java
@@ -6,44 +6,29 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 class A2798_2 {
-	public static int c1 = 0, c2 = 1, c3 = 2, n;
-	
-	public static boolean swap() {
-		c3++;
-		if(c3 == n) {
-			c2++;
-			c3 = c2 + 1;
-			if(c2 == n-1) {
-				c1++;
-				if(c1 == n-2) return false;
-				c2 = c1 + 1;
-				c3 = c2 + 1;					
-			}
-		}
-		return true;
-	}
-	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
 		int[] arr = new int[n];
 		st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < n; i++) arr[i] = Integer.parseInt(st.nextToken());
 		
-		int answer = -1;
-		while(true) {
-			int sum = arr[c1] + arr[c2] + arr[c3];
-			if(sum > m) {
-				boolean result = swap();
-				if(result) continue;
-				else break;
+		int answer = 0;
+		for(int i = 0; i < n-2; i++) {
+			for(int j = i+1; j < n-1; j++) {
+				for(int k = j+1; k < n; k++) {
+					int sum = arr[i] + arr[j] + arr[k];
+					if(sum == m) {
+						System.out.println(m);
+						return;
+					}
+					if(sum < m) {
+						answer = Math.max(answer, sum);
+					}
+				}
 			}
-			System.out.println(c1 + " / " + c2 + " / " + c3);
-			if(Math.abs(m - answer) > Math.abs(m - sum)) answer = sum;
-			if(answer == m) break;
-			if(!swap()) break;
 		}
 		System.out.println(answer);
 	}
