ee2d904f4fe61f1213b40d2279bf817ba8e64767
MiraeChoi <pnenv@naver.com>
2022-08-13 10:59:08 +0900

    add qnas

--- src/k_backjoon/A2750.java
+++ src/k_backjoon/A2750.java
@@ -1,7 +1,17 @@
 package k_backjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 class A2750 {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		for(int i = 0; i < n; i++) arr[i] = Integer.parseInt(br.readLine());
+		Arrays.sort(arr);
+		for(int i = 0; i < n; i++) System.out.println(arr[i]);
 	}
 }
