d7cc39021c5c6e5dc0b9275443f5b17a488e4a21
MiraeChoi <pnenv@naver.com>
2022-08-18 15:35:23 +0900

    add qnas

---
+++ src/k_backjoon/A10989.java
@@ -0,0 +1,21 @@
+package k_backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+class A10989 {
+	//맞았습니다!!
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		for(int i = 0; i < n; i++) arr[i] = Integer.parseInt(br.readLine());
+		Arrays.sort(arr);
+		for(int i = 0; i < n; i++) sb.append(arr[i] + "\n");
+		System.out.println(sb);
+		br.close();
+	}
+}
