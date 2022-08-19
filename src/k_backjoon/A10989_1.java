de8abb5780aea47c73a9d1c58ca1752be37b4ec7
MiraeChoi <pnenv@naver.com>
2022-08-19 23:50:06 +0900

    add qnas

--- src/k_backjoon/A10989_1.java
+++ src/k_backjoon/A10989_1.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-class A10989 {
+class A10989_1 {
 	//맞았습니다!!
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
---
+++ src/k_backjoon/A10989_2.java
@@ -0,0 +1,32 @@
+package k_backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class A10989_2 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		int max = Integer.MIN_VALUE;
+		for(int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+			max = Math.max(max, arr[i]);
+		}
+		
+		int[] count = new int[max + 1];
+		for(int i = 0; i < n; i++) count[arr[i]]++;
+		for(int i = 1; i < count.length; i++) count[i] = count[i-1] + count[i];
+		
+		int[] result = new int[n];
+		for(int i = n-1; i >= 0; i--) {
+			int tmp = count[arr[i]]--;
+			result[--tmp] = arr[i];
+		}
+		for(int i = 0; i < n; i++) sb.append(result[i] + "\n");
+		System.out.println(sb);
+		br.close();
+	}
+}
