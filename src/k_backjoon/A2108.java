a4654a14ee25a958b3ae97fdad6dc5e1720694ff
MiraeChoi <pnenv@naver.com>
2022-08-21 22:46:50 +0900

    add qnas

---
+++ src/k_backjoon/A2108.java
@@ -0,0 +1,7 @@
+package k_backjoon;
+
+class A2108 {
+	public static void main(String[] args) {
+		
+	}
+}
--- src/k_backjoon/A25305_2.java
+++ src/k_backjoon/A25305_2.java
@@ -3,6 +3,8 @@ package k_backjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
 class A25305_2 {
@@ -12,8 +14,9 @@ class A25305_2 {
 		int n = Integer.parseInt(st.nextToken());
 		int k = Integer.parseInt(st.nextToken());
 		st = new StringTokenizer(br.readLine());
-		int[] arr = new int[n];
+		Integer[] arr = new Integer[n];
 		for(int i = 0; i < n; i++) arr[i] = Integer.parseInt(st.nextToken());
-		
+		Arrays.sort(arr, Collections.reverseOrder());
+		System.out.println(arr[k-1]);
 	}
 }
