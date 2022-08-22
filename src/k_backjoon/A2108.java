b4e072a7445daed6ce35c756c92e5a68d0e85d17
MiraeChoi <pnenv@naver.com>
2022-08-22 23:59:23 +0900

    add qnas

--- src/k_backjoon/A2108.java
+++ src/k_backjoon/A2108.java
@@ -1,7 +1,33 @@
 package k_backjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 class A2108 {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		int sum = 0, mid = 0, min = Integer.MAX_VALUE, range = 0;
+		for(int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+			sum += arr[i];
+			min = Math.min(min, arr[i]);
+		}
+		Arrays.sort(arr);
+		System.out.println(sum / n);
+		System.out.println(arr[n/2]);
+		if(n == 1 || min != arr[1]) System.out.println(min);
+		else {
+			for(int i = 0; i< n; i++) {
+				if(min != arr[i]) {
+					System.out.println(arr[i]);
+					break;
+				}
+			}
+		}
+		System.out.println(arr[n-1] - arr[0]);
 	}
 }
