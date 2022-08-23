2aa02aadfe8d227abff7c8e8554d45a2d0de51e4
MiraeChoi <pnenv@naver.com>
2022-08-23 15:21:42 +0900

    edit my answer

--- src/k_backjoon/A2108.java
+++ src/k_backjoon/A2108.java
@@ -10,23 +10,29 @@ class A2108 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		int[] arr = new int[n];
-		int sum = 0, mid = 0, min = Integer.MAX_VALUE, range = 0;
+		int sum = 0, mid = 0, mode = 0, range = 0;
 		for(int i = 0; i < n; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
 			sum += arr[i];
-			min = Math.min(min, arr[i]);
 		}
 		Arrays.sort(arr);
-		System.out.println(sum / n);
+		System.out.println(Math.round((float)sum / (float)n));
 		System.out.println(arr[n/2]);
-		if(n == 1 || min != arr[1]) System.out.println(min);
-		else {
-			for(int i = 0; i< n; i++) {
-				if(min != arr[i]) {
-					System.out.println(arr[i]);
-					break;
+		//최빈값을 출력한다. 여러 개 있을 때에는 최빈값 중 두 번째로 작은 값을 출력한다.
+		mode = arr[0];
+		int cnt = 0;
+		for(int i = 0; i < n-1; i++) {
+			if(arr[i] == arr[i+1]) {
+				while(arr[i] == arr[i+1]) {
+					cnt++;
+					i++;
 				}
 			}
+			if(cnt == 0) {
+				System.out.println(mode);
+				break;
+			}
+			cnt = 0;
 		}
 		System.out.println(arr[n-1] - arr[0]);
 	}
