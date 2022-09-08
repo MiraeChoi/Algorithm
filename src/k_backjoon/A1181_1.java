ff0d17ad38c114906b05a395cacc44daf18acd64
MiraeChoi <pnenv@naver.com>
2022-09-09 07:58:18 +0900

    add a new class

--- src/k_backjoon/A1181_1.java
+++ src/k_backjoon/A1181_1.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-class A1181 {
+class A1181_1 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
