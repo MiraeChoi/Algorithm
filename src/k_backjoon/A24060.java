9fb0e5d1eb81159590be7e0a1dc63d26f526048e
MiraeChoi <pnenv@naver.com>
2022-09-20 18:15:57 +0900

    edit my answer

--- src/k_backjoon/A24060.java
+++ src/k_backjoon/A24060.java
@@ -6,6 +6,27 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 class A24060 {
+	public static void merge_sort(int[] arr, int p, int r) {
+		if(p < r) {
+			int q = (p + r) / 2;
+			merge_sort(arr, p, q);
+			merge_sort(arr, q + 1, r);
+			merge(arr, p, q, r);
+		}
+	}
+	
+	public static void merge(int[] arr, int p, int q, int r) {
+		int i = p, j = q + 1, t = 1;
+		int[] tmp = new int[arr.length];
+		while(i <= q && j <= r) {
+			if(arr[i] <= arr[j]) tmp[t++] = arr[i++];
+			else tmp[t++] = arr[j++];
+		}
+		while(i <= q) tmp[t++] = arr[i++];
+		while(j <= r) tmp[t++] = arr[j++];
+		while(i <= r) arr[i++] = tmp[t++];
+	}
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -14,13 +35,6 @@ class A24060 {
 		int[] arr = new int[n];
 		st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < n; i++) arr[i] = Integer.parseInt(st.nextToken());
-		while(k >= 0) {
-			int cnt = 0;
-			int cur = arr[cnt];
-			for(int i = arr[0]; i < n; i++) {
-				
-			}
-			k--;
-		}
+		merge_sort(arr, 0, n-1);
 	}
 }
