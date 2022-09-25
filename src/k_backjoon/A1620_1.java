40928a1e54614bf1d040b5b25836969f75e846a3
MiraeChoi <pnenv@naver.com>
2022-09-25 23:59:28 +0900

    edit my answer

--- src/k_backjoon/A1620_1.java
+++ src/k_backjoon/A1620_1.java
@@ -1,7 +1,22 @@
 package k_backjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 class A1620_1 {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		String[] poke = new String[n];
+		for(int i = 0; i < n; i++) {
+			poke[i] = br.readLine();
+		}
+		for(int i = 0; i < m; i++) {
+			
+		}
 	}
 }
