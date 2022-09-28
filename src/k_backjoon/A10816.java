f54b6ea5a1734ac3cdd05b42f9cf03223cb0eadc
MiraeChoi <pnenv@naver.com>
2022-09-28 23:59:22 +0900

    edit my answer

--- src/k_backjoon/A10816.java
+++ src/k_backjoon/A10816.java
@@ -1,7 +1,23 @@
 package k_backjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 class A10816 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] card = new int[n];
+		for(int i = 0; i < n; i++) card[i] = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(br.readLine());
+		int[] arr = new int[m];
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < m; i++) {
+			int num = Integer.parseInt(st.nextToken());
+		}
 		
 	}
 }
