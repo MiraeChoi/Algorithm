032bd2d025dfbf07cd6f676d9e7c49d48bbc2274
MiraeChoi <pnenv@naver.com>
2022-10-09 23:59:45 +0900

    edit my answer

--- src/k_backjoon/A11478.java
+++ src/k_backjoon/A11478.java
@@ -1,7 +1,20 @@
 package k_backjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 class A11478 {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		int n = str.length();
+		int answer = 0;
+		for(int i = 1; i <= n; i++) answer += i;
+		for(int i = 1; i <= n; i++) {
+			for(int j = 1; j < i; j++) {
+				
+			}
+		}
 	}
 }
