b91e75042dcab639fc7171f183002f33d05ccbc4
MiraeChoi <pnenv@naver.com>
2022-10-10 23:59:44 +0900

    edit my answer

--- src/k_backjoon/A11478.java
+++ src/k_backjoon/A11478.java
@@ -3,6 +3,8 @@ package k_backjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 
 class A11478 {
 	public static void main(String[] args) throws IOException {
@@ -12,8 +14,10 @@ class A11478 {
 		int answer = 0;
 		for(int i = 1; i <= n; i++) answer += i;
 		for(int i = 1; i <= n; i++) {
-			for(int j = 1; j < i; j++) {
-				
+			Queue<Character> Q = new LinkedList<>();
+			for(int j = 1; j <= i; j++) {
+				if(Q.contains(str.substring(j, j+i))) answer--;
+//				else Q.add(str.substring(j, j+i)));
 			}
 		}
 	}
