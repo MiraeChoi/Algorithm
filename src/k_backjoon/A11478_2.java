e447686ce87d6467d8785dfaad463860afbaeb6c
MiraeChoi <pnenv@naver.com>
2022-10-12 23:59:16 +0900

    edit my answer

--- src/k_backjoon/A11478_2.java
+++ src/k_backjoon/A11478_2.java
@@ -11,28 +11,21 @@ public class A11478_2 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		String input = br.readLine();
-		int length = input.length();
-		String[] suff_arr = new String[length];
+		String str = br.readLine();
+		int len = str.length();
+		String[] arr = new String[len];
 		int lcp = 0;
-		for (int i = 0; i < length; i++) {
-			suff_arr[i] = input.substring(length - 1 - i);
-		}
-		Arrays.sort(suff_arr);
-		for (int i = 1; i < length; i++) {
-			for (int j = 0; j < suff_arr[i - 1].length(); j++) {
-				if(suff_arr[i-1].charAt(j) == suff_arr[i].charAt(j)) {
-					lcp++;
-				}
-				else {
-					break;
-				}
+		for(int i = 0; i < len; i++) arr[i] = str.substring(len - 1 - i);
+		Arrays.sort(arr);
+		for(int i = 0; i < len; i++) System.out.println(arr[i]);
+		for(int i = 1; i < len; i++) {
+			for(int j = 0; j < arr[i - 1].length(); j++) {
+				if(arr[i-1].charAt(j) == arr[i].charAt(j)) lcp++;
+				else break;
 			}
 		}
-		int ans = (length*(length+1)/2-lcp);
-		bw.write(String.valueOf(ans));
+		int answer = (len * (len + 1) / 2 - lcp);
+		bw.write(answer + "");
 		bw.flush();
-		bw.close();
-		br.close();
 	}
 }
