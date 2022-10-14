7cfc5f2cee44257ded5b484f45cc62bce0521a7c
MiraeChoi <pnenv@naver.com>
2022-10-14 16:38:52 +0900

    add qnas

--- src/k_backjoon/A1085.java
+++ src/k_backjoon/A1085.java
@@ -5,7 +5,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-class A1085_1 {
+class A1085 {
+	//맞았습니다!!
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -13,10 +14,7 @@ class A1085_1 {
 		int y = Integer.parseInt(st.nextToken());
 		int w = Integer.parseInt(st.nextToken());
 		int h = Integer.parseInt(st.nextToken());
-		int answer = 0;
-		answer = Math.min(Math.abs(x-w), Math.abs(x-0));
-		answer = Math.min(answer, Math.abs(y-h));
-		answer = Math.min(answer, Math.abs(y-0));
+		int answer = Math.min(x, Math.min(y, Math.min(Math.abs(x-w), Math.abs(y-h))));
 		System.out.println(answer);
 	}
 }
---
+++ src/k_backjoon/A3009.java
@@ -0,0 +1,22 @@
+package k_backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class A3009 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int a1 = Integer.parseInt(st.nextToken());
+		int a2 = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		int b1 = Integer.parseInt(st.nextToken());
+		int b2 = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		int c1 = Integer.parseInt(st.nextToken());
+		int c2 = Integer.parseInt(st.nextToken());
+		
+	}
+}
