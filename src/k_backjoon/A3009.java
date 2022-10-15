ee2abf22d957cdb22b4255a39ae59e05533c2d6e
MiraeChoi <pnenv@naver.com>
2022-10-15 23:59:28 +0900

    edit my answer

--- src/k_backjoon/A3009.java
+++ src/k_backjoon/A3009.java
@@ -5,7 +5,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class A3009 {
+class A3009 {
+	//맞았습니다!!
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -18,5 +19,13 @@ public class A3009 {
 		int c1 = Integer.parseInt(st.nextToken());
 		int c2 = Integer.parseInt(st.nextToken());
 		
+		int answer1 = 0, answer2 = 0;
+		if(a1 == b1) answer1 = c1;
+		else if(b1 == c1) answer1 = a1;
+		else answer1 = b1;
+		if(a2 == b2) answer2 = c2;
+		else if(b2 == c2) answer2 = a2;
+		else answer2 = b2;
+		System.out.println(answer1 + " " + answer2);
 	}
 }
