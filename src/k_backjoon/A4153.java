f2c07f6f902245658c0ee876cb1dac29f34cae93
MiraeChoi <pnenv@naver.com>
2022-10-18 18:22:50 +0900

    add qnas

---
+++ src/k_backjoon/A4153.java
@@ -0,0 +1,22 @@
+package k_backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class A4153 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		while(true) {
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			if(a == 0 && b == 0 && c == 0) break;
+			else {
+				//직각삼각형 알아내기
+			}
+		}
+	}
+}
