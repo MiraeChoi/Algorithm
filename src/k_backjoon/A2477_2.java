449ab1bb508b31c03c45dea35a6faf8c4c4c2abd
MiraeChoi <pnenv@naver.com>
2022-10-22 23:28:10 +0900

    edit my answer

--- src/k_backjoon/A2477_2.java
+++ src/k_backjoon/A2477_2.java
@@ -3,37 +3,39 @@ package k_backjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
-class Arr2447_2 {
-	int pointer, length;
-	
-	public Arr2447_2(int pointer, int length) {
-		this.pointer = pointer;
-		this.length = length;
-	}
-}
-
 class A2477_2 {
+	//맞았습니다!!
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int K = Integer.parseInt(br.readLine());
 		StringTokenizer st;
-		Queue<Arr2447_2> Q = new LinkedList<>();
-		int[] idxCheck = new int[4+1];
+		int[] pointer = new int[6+5];
+		int[] length = new int[6+5];
 		
 		for(int i = 0; i < 6; i++) {
 			st = new StringTokenizer(br.readLine());
 			int p = Integer.parseInt(st.nextToken());
 			int l = Integer.parseInt(st.nextToken());
-			Q.offer(new Arr2447_2(p, l));
-			if(idxCheck[p] > 0) {
-				
-			}
-			else idxCheck[p]++;
+			pointer[i] = p;
+			length[i] = l;
 		}
-		System.out.println();
+		for(int i = 0; i < 6; i++) {
+			if(pointer[i] == pointer[i+2] && pointer[i+1] == pointer[i+3]) {
+				break;
+			} else {
+				pointer[i+6] = pointer[i];
+				pointer[i] = 0;
+				length[i+6] = length[i];
+				length[i] = 0;
+			}
+		}
+		for(int i = 0; i < pointer.length; i++) {
+			if(pointer[i] != 0 && length[i] != 0) {
+				System.out.println((length[i+4] * length[i+5] - length[i+1] * length[i+2]) * K);
+				break;
+			}
+		}
 	}
 }
