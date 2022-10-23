cab29dc6db99ed9a4cb4f09a3d20137cb2f47bb4
MiraeChoi <pnenv@naver.com>
2022-10-23 23:21:53 +0900

    add a new class

--- src/k_backjoon/A1002_1.java
+++ src/k_backjoon/A1002_1.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-class A1002 {
+class A1002_1 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
