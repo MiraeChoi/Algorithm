a175a4e136c8045ee1bb9dfdd5ddb029b33a159e
MiraeChoi <pnenv@naver.com>
2022-10-24 23:59:21 +0900

    edit my answer

--- src/k_backjoon/A1002_2.java
+++ src/k_backjoon/A1002_2.java
@@ -1,7 +1,18 @@
 package k_backjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 class A1002_2 {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int T = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < T; i++) {
+			
+		}
 	}
 }
