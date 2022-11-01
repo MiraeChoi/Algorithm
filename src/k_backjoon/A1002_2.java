3e64d94c3244789ca95db440bbeead88df95d2f0
MiraeChoi <pnenv@naver.com>
2022-11-01 14:17:15 +0900

    edit my answer

--- src/k_backjoon/A1002_2.java
+++ src/k_backjoon/A1002_2.java
@@ -8,11 +8,25 @@ import java.util.StringTokenizer;
 class A1002_2 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
 		int T = Integer.parseInt(br.readLine());
+		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
 		for(int i = 0; i < T; i++) {
+			st = new StringTokenizer(br.readLine());
+			int x1 = Integer.parseInt(st.nextToken());
+			int y1 = Integer.parseInt(st.nextToken());
+			int r1 = Integer.parseInt(st.nextToken());
+			int x2 = Integer.parseInt(st.nextToken());
+			int y2 = Integer.parseInt(st.nextToken());
+			int r2 = Integer.parseInt(st.nextToken());
 			
+			if(x1 == x2 && y1 == y2 && r1 == r2) sb.append(-1).append('\n');
+			else if(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2) > Math.pow(r1 + r2, 2)
+					|| Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2) < Math.pow(r2 - r1, 2)) sb.append(0).append('\n');
+			else if(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2) == Math.pow(r2 - r1, 2)
+					|| Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2) == Math.pow(r1 + r2, 2)) sb.append(1).append('\n');
+			else sb.append(2).append('\n');
 		}
+		System.out.println(sb);
 	}
 }
--- src/k_backjoon/A3053.java
+++ src/k_backjoon/A3053.java
@@ -7,8 +7,10 @@ import java.io.InputStreamReader;
 class A3053 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int R = Integer.parseInt(br.readLine());
-		System.out.println(R * R * 3.141593);
-		System.out.println(String.format("%.6f", (float)((R * 2) * (R * 2) / 2)));
+		double R = Double.parseDouble(br.readLine());
+//		System.out.println(String.format("%.6f", Math.PI * (R * R)));
+//		System.out.println(String.format("%.6f", (double)((R * 2) * (R * 2) / 2)));
+		System.out.println(R * R * Math.PI);
+		System.out.println(R * R * 2);
 	}
 }
