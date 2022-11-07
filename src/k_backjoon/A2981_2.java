b8a0e636361dac5a719446738568f545c3c290d8
MiraeChoi <pnenv@naver.com>
2022-11-07 17:26:07 +0900

    edit my answer

--- src/k_backjoon/A2981_2.java
+++ src/k_backjoon/A2981_2.java
@@ -10,16 +10,17 @@ class A2981_2 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		int[] arr = new int[N];
-		int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
-		for(int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
-			max = Math.max(max, arr[i]);
-			min = Math.min(min, arr[i]);
-		}
+		for(int i = 0; i < N; i++) arr[i] = Integer.parseInt(br.readLine());
+		int idx = 0;
+		int min = arr[0], max = arr[N-1];
 		StringBuilder sb = new StringBuilder();
 		out : for(int i = 2; i <= max; i++) {
 			int rem = min % i;
-//			if(i > min && rem != min) continue; 인덱스 지정해서 min을 넘을 때마다 idx++하면...?
+			if(i > min && rem != min) {
+				idx++;
+				if(idx == N-1) break;
+				else min = arr[idx];
+			}
 			for(int j = 1; j < N; j++) {
 				if(arr[j] % i != rem) continue out;
 			}
