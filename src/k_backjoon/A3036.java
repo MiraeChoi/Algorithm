ead7acca8d0362c42f4721269882415b3c42c519
MiraeChoi <pnenv@naver.com>
2022-11-09 17:41:52 +0900

    edit my answer

--- src/k_backjoon/A3036.java
+++ src/k_backjoon/A3036.java
@@ -1,7 +1,22 @@
 package k_backjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 class A3036 {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < N; i++) {
+			int A = 0, B = 0;
+			sb.append(A).append('/').append(B).append('\n');
+		}
+		System.out.println(sb);
 	}
 }
