12207a01affb1003ddfeee515313cb96d806358d
MiraeChoi <pnenv@naver.com>
2022-11-11 23:47:31 +0900

    edit my answer

--- src/k_backjoon/A3036_1.java
+++ src/k_backjoon/A3036_1.java
@@ -6,6 +6,27 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 class A3036_1 {
+	//Æ²·È½À´Ï´Ù
+	public static int[] divisor(int a, int b) {
+		int[] divisors = new int[2];
+		if(a % b == 0) {
+			divisors[0] = a / b;
+			divisors[1] = 1;
+		} else if(b % a == 0) {
+			divisors[0] = 1;
+			divisors[1] = b / a;
+		} else {
+			for(int i = Math.min(a, b)-1; i >= 2; i--) {
+				if(a % i == 0 && b % i == 0) {
+					divisors[0] = a / i;
+					divisors[1] = b / i;
+					break;
+				}
+			}
+		}
+		return divisors;
+	}
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
@@ -13,9 +34,9 @@ class A3036_1 {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
 		StringBuilder sb = new StringBuilder();
-		for(int i = 0; i < N; i++) {
-			int A = 0, B = 0;
-			sb.append(A).append('/').append(B).append('\n');
+		for(int i = 1; i < N; i++) {
+			int[] answers = divisor(arr[0], arr[i]);
+			sb.append(answers[0]).append('/').append(answers[1]).append('\n');
 		}
 		System.out.println(sb);
 	}
