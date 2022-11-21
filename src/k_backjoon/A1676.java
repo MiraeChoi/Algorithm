417264252c37060415de096af98f3555816cc977
MiraeChoi <pnenv@naver.com>
2022-11-21 23:59:32 +0900

    edit my answer

--- src/k_backjoon/A1676.java
+++ src/k_backjoon/A1676.java
@@ -1,7 +1,17 @@
 package k_backjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 class A1676 {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int answer = 0;
+		for(int i = N; i >= 0; i--) {
+			
+		}
+		System.out.println(answer);
 	}
 }
