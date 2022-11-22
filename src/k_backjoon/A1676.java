e4c192e56fcb9730a4576ff96705dd7f53865ac8
MiraeChoi <pnenv@naver.com>
2022-11-22 18:15:02 +0900

    edit my answer

--- src/k_backjoon/A1676.java
+++ src/k_backjoon/A1676.java
@@ -3,14 +3,21 @@ package k_backjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 
 class A1676 {
+	//1 ~ 500!
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
+		long sum = 1;
+		for(int i = N; i >= 1; i--) sum *= i;
+		BigInteger big = new BigInteger(sum + "");
+		String str = sum + "";
 		int answer = 0;
-		for(int i = N; i >= 0; i--) {
-			
+		System.out.println("sum : " + sum);
+		for(int i = 0; i < str.length(); i++) {
+			if(str.charAt(i) == '0') answer++;
 		}
 		System.out.println(answer);
 	}
