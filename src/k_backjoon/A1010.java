00ff4deee663d51f840abdc1b8ada60c68bc80ef
MiraeChoi <pnenv@naver.com>
2022-11-28 23:59:41 +0900

    add a new class

---
+++ src/k_backjoon/A1010.java
@@ -0,0 +1,17 @@
+package k_backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class A1010 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < T; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+		}
+	}
+}
--- src/k_backjoon/A11051_1.java
+++ src/k_backjoon/A11051_1.java
@@ -10,6 +10,13 @@ class A11051_1 {
 	//¹İ·Ê : 1000 259
 	static int[][] arr;
 	
+	static int BC(int n, int k) {
+		if(arr[n][k] > 0) return arr[n][k];
+		else if(k == 0 || n == k) return arr[n][k] = 1;
+		else if(k == 1) return arr[n][k] = n;
+		return arr[n][k] = BC(n-1, k-1) + BC(n-1, k);
+	}
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
@@ -17,12 +24,5 @@ class A11051_1 {
 		int K = Integer.parseInt(st.nextToken());
 		arr = new int[N+1][K+1];
 		System.out.println(BC(N, K) % 10007);
-	}
-	
-	static int BC(int n, int k) {
-		if(arr[n][k] > 0) return arr[n][k];
-		else if(k == 0 || n == k) return arr[n][k] = 1;
-		else if(k == 1) return arr[n][k] = n;
-		return arr[n][k] = BC(n-1, k-1) + BC(n-1, k);
 	}
 }
