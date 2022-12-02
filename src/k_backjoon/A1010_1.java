19b4b80ab2a41906a8349af8c3ac33b61d679bba
MiraeChoi <pnenv@naver.com>
2022-12-02 23:59:46 +0900

    edit my answer

--- src/k_backjoon/A1010_1.java
+++ src/k_backjoon/A1010_1.java
@@ -6,10 +6,20 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class A1010_1 {
+	static int N, M;
+	
 	private static int factorial(int n) {
 		int answer = 1;
 		for(int i = n; i >= 2; i--) answer *= i;
 		return answer;
+	}
+	
+	private static int factorial2(int n, int m) {
+		int answer = 1;
+		for(int i = m; i > m - n; i--) answer *= i;
+		System.out.println("factorial(n) : " + factorial(n));
+		System.out.println("factorial(m) : " + factorial(m));
+		return factorial(n) / answer;
 	}
 	
 	public static void main(String[] args) throws IOException {
@@ -19,12 +29,12 @@ public class A1010_1 {
 		StringTokenizer st;
 		for(int i = 0; i < T; i++) {
 			st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken());
-			int M = Integer.parseInt(st.nextToken());
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
 			if(N == M) sb.append(1).append('\n');
 			else if(N == 1) sb.append(M).append('\n');
 			else if(M == 1) sb.append(N).append('\n');
-			else sb.append(factorial(M - N - 5) * 2).append('\n');
+			else sb.append(factorial2(N, M)).append('\n');
 		}
 		System.out.println(sb);
 	}
