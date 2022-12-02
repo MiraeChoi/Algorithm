17adce0587f21488e9912482aeb7e17cddc25e1d
MiraeChoi <pnenv@naver.com>
2022-12-03 01:26:31 +0900

    add qnas

--- src/k_backjoon/A1010_1.java
+++ src/k_backjoon/A1010_1.java
@@ -3,23 +3,19 @@ package k_backjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.util.StringTokenizer;
 
-public class A1010_1 {
-	static int N, M;
-	
-	private static int factorial(int n) {
-		int answer = 1;
-		for(int i = n; i >= 2; i--) answer *= i;
-		return answer;
-	}
-	
-	private static int factorial2(int n, int m) {
-		int answer = 1;
-		for(int i = m; i > m - n; i--) answer *= i;
-		System.out.println("factorial(n) : " + factorial(n));
-		System.out.println("factorial(m) : " + factorial(m));
-		return factorial(n) / answer;
+class A1010_1 {
+	//맞았습니다!!
+	private static int solution(int n, int m) {
+		BigInteger nu = new BigInteger("1");
+		BigInteger de = new BigInteger("1");
+		for(int i = m; i > n; i--) nu = nu.multiply(new BigInteger(i + ""));
+		for(int i = m - n; i >= 2; i--) de = de.multiply(new BigInteger(i + ""));
+		System.out.println(nu + " / " + de);
+		BigInteger answer = nu.divide(de);
+		return answer.intValue();
 	}
 	
 	public static void main(String[] args) throws IOException {
@@ -29,12 +25,12 @@ public class A1010_1 {
 		StringTokenizer st;
 		for(int i = 0; i < T; i++) {
 			st = new StringTokenizer(br.readLine());
-			N = Integer.parseInt(st.nextToken());
-			M = Integer.parseInt(st.nextToken());
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
 			if(N == M) sb.append(1).append('\n');
 			else if(N == 1) sb.append(M).append('\n');
 			else if(M == 1) sb.append(N).append('\n');
-			else sb.append(factorial2(N, M)).append('\n');
+			else sb.append(solution(N, M)).append('\n');
 		}
 		System.out.println(sb);
 	}
--- src/k_backjoon/A1010_2.java
+++ src/k_backjoon/A1010_2.java
@@ -1,5 +1,42 @@
 package k_backjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 class A1010_2 {
+	private static int nCr(int n, int r){
+		if(r * 2 > n) r = n - r;
+		int[] arr = new int[r+1];
+		for(int m = 0; m <= r; m++) {
+			for(int k = m-1; k > 0; k--) {
+				arr[k] += arr[k-1];
+			}
+			arr[m] = 1;
+		}
+		for(int i = 0; i < arr.length; i++) System.out.println(arr[i] + "/");
+		System.out.println("---------------------------");
+		for(int m = r+1; m <= n; m++) {
+			for(int k = r; k > 0; k--) {
+				arr[k] += arr[k-1];
+			}
+		}
+		for(int i = 0; i < arr.length; i++) System.out.println(arr[i] + "/");
+		return arr[r];
+	}
 	
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		while(T-- > 0) {
+			st = new StringTokenizer(br.readLine());
+			int n = Integer.parseInt(st.nextToken());
+			int m = Integer.parseInt(st.nextToken());
+			sb.append(nCr(m, n) + "\n");
+		}
+		System.out.println(sb);
+	}
 }
