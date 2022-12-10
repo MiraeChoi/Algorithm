31a5becd486eca3abfcb26d80d34cc7b8fac0b80
MiraeChoi <pnenv@naver.com>
2022-12-10 15:25:57 +0900

    add qnas

--- src/k_backjoon/A2805.java
+++
@@ -1,7 +0,0 @@
-package k_backjoon;
-
-class A2805 {
-	public static void main(String[] args) {
-		
-	}
-}
---
+++ src/k_backjoon/A2805_1.java
@@ -0,0 +1,35 @@
+package k_backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class A2805_1 {
+	//시간 초과
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[] arr = new int[N];
+		st = new StringTokenizer(br.readLine(), " ");
+		int answer = 0;
+		int MAX = Integer.MIN_VALUE;
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			MAX = Math.max(MAX, arr[i]);
+		}
+		for(int i = MAX; i >= 1; i--) {
+			int cnt = 0;
+			for(int j = 0; j < N; j++) {
+				if(arr[j] - i > 0) cnt += arr[j] - i;
+			}
+			if(cnt >= M) {
+				answer = i;
+				break;
+			}
+		}
+		System.out.println(answer);
+	}
+}
