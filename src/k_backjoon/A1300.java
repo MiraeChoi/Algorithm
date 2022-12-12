6740f24ed2f5388c30290bc0d6b32076f0cf9b21
MiraeChoi <pnenv@naver.com>
2022-12-13 02:01:21 +0900

    edit my answer

--- src/k_backjoon/A1300.java
+++ src/k_backjoon/A1300.java
@@ -3,18 +3,27 @@ package k_backjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 class A1300 {
+	//메모리 초과
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		int k = Integer.parseInt(br.readLine());
-		int[][] A = new int[N][N];
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < N; j++) {
-				A[i][j] = i * j;
+		int[][] A = new int[N+1][N+1];
+		for(int i = 1; i <= N; i++) {
+			for(int j = 1; j <= N; j++) A[i][j] = i * j;
+		}
+		int[] B = new int[k+1];
+		int idx = 1;
+		out : for(int i = 1; i <= N; i++) {
+			for(int j = 1; j <= N; j++) {
+				B[idx] = A[i][j];
+				if(++idx > k) break out;
 			}
 		}
-		System.out.println(A[k]);
+		Arrays.sort(B);
+		System.out.println(B[k]);
 	}
 }
