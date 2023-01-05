52a6873170fcf219355eb47f47ce580b1146b264
MiraeChoi <pnenv@naver.com>
2023-01-06 02:05:57 +0900

    edit my answer

--- src/k_backjoon/A2580_2.java
+++ src/k_backjoon/A2580_2.java
@@ -13,18 +13,17 @@ class A2580_2 {
 			for(int i = 0; i < sudoku.length; i++) {
 				if(sudoku[L][i] > 0) arr[sudoku[L][i]]++;
 				else {
-					canDo(L);
+					findNum(i);
 					solution(L + 1);
 				}
 			}
 		}
 	}
 	
-	private static boolean canDo(int col) {
+	private static void findNum(int idx) {
 		for(int i = 0; i < sudoku.length; i++) {
-			
+			if(arr[i] == 0) sudoku[idx][i] = idx;
 		}
-		return true;
 	}
 	
 	public static void main(String[] args) throws IOException {
