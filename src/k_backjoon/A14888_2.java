36294810015128e774cab7190b9c05725b353735
MiraeChoi <pnenv@naver.com>
2023-01-09 23:59:59 +0900

    edit my answer

--- src/k_backjoon/A14888_2.java
+++ src/k_backjoon/A14888_2.java
@@ -1,7 +1,21 @@
 package k_backjoon;
 
+import java.io.IOException;
+
 class A14888_2 {
-	public static void main(String[] args) {
+	static int[] op = {0, 0, 0, 0};
+	
+	public static void main(String[] args) throws IOException {
+		int N = readInt();
 		
+	}
+	
+	private static int readInt() throws IOException {
+		int n = 0;
+		while(true) {
+			int input = System.in.read();
+			if(input <= 32) return n;
+			else n = (n << 3) + (n << 1) + (input & 15);
+		}
 	}
 }
