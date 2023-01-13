3883adaa36e53e442fe330857627b19a62b40c49
MiraeChoi <pnenv@naver.com>
2023-01-13 23:59:18 +0900

    add qnas

---
+++ src/k_backjoon/A14889_1.java
@@ -0,0 +1,56 @@
+package k_backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class A14889_1 {
+	//맞았습니다!!
+	static int N, answer = Integer.MAX_VALUE;
+	static int[][] arr;
+	static boolean[] team;
+	
+	private static void DFS(int idx, int L) {
+		if(L == N / 2) {
+			SUM();
+			return;
+		} else {
+			for(int i = idx; i < N; i++) {
+				if(!team[i]) team[i] = true;
+				DFS(i + 1, L + 1);
+				team[i] = false;
+			}
+		}
+	}
+	
+	private static void SUM() {
+		int start = 0, link = 0;
+		for(int i = 0; i < N - 1; i++) {
+			for(int j = i + 1; j < N; j++) {
+				if(team[i] && team[j]) {
+					start += arr[i][j] + arr[j][i];
+				}
+				if(!team[i] && !team[j]) {
+					link += arr[i][j] + arr[j][i];
+				}
+			}
+		}
+		answer = Math.min(answer, Math.abs(start - link));
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		arr = new int[N][N];
+		for(int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			for(int j = 0; j < N; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		team = new boolean[N];
+		DFS(0, 0);
+		System.out.println(answer);
+	}
+}
--- src/k_backjoon/A14889_2.java
+++ src/k_backjoon/A14889_2.java
@@ -5,20 +5,40 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-class A14889 {
+class A14889_2 {
 	static int N, answer = Integer.MAX_VALUE;
 	static int[][] arr;
 	static boolean[] team;
 	
-	private static void DFS(int L) {
+	private static void DFS(int idx, int L) {
 		if(L == N / 2) {
-			
+			SUM();
+			return;
 		} else {
-			for(int i = 0; i < N; i++) {
+			for(int i = idx; i < N; i++) {
 				if(!team[i]) team[i] = true;
-				DFS(L + 1);
+				DFS(i + 1, L + 1);
 				team[i] = false;
 			}
+		}
+	}
+	
+	private static void SUM() {
+		int start = 0, link = 0;
+		for(int i = 0; i < N - 1; i++) {
+			for(int j = i + 1; j < N; j++) {
+				if(team[i] && team[j]) {
+					start += arr[i][j] + arr[j][i];
+				}
+				if(!team[i] && !team[j]) {
+					link += arr[i][j] + arr[j][i];
+				}
+			}
+		}
+		answer = Math.min(answer, Math.abs(start - link));
+		if(answer == 0) {
+			System.out.println(answer);
+			System.exit(0);
 		}
 	}
 	
@@ -33,7 +53,7 @@ class A14889 {
 			}
 		}
 		team = new boolean[N];
-		DFS(0);
+		DFS(0, 0);
 		System.out.println(answer);
 	}
 }
