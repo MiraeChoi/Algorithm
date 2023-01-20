91b96b561ac3fe11ebf78bf01936a04b6cd0bdb1
MiraeChoi <pnenv@naver.com>
2023-01-21 01:42:55 +0900

    edit my answer

--- src/k_backjoon/A10828.java
+++ src/k_backjoon/A10828.java
@@ -4,35 +4,34 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
-import java.util.StringTokenizer;
 
 class A10828 {
+	//맞았습니다!!
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		Stack<Integer> stack = new Stack<>();
 		StringBuilder sb = new StringBuilder();
 		for(int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			String str = st.nextToken();
-			switch(str) {
-			case "push" :
-				int n = Integer.parseInt(st.nextToken());
+			String str = br.readLine();
+			if(str.startsWith("push")) {
+				int n = Integer.parseInt((String)str.substring(5));
 				stack.push(n);
-				sb.append(n).append('\n');
-				return;
-			case "pop" :
-				sb.append(stack.pop()).append('\n');
-				return;
-			case "size" :
-				sb.append(stack.size()).append('\n');
-				return;
-			case "empty" :
-				sb.append(stack.empty() ? 1 : 0).append('\n');
-				return;
-			case "top" :
-				sb.append(stack.peek()).append('\n');
-				return;
+			} else {
+				switch(str) {
+				case "pop" :
+					sb.append(stack.empty() ? -1 : stack.pop()).append('\n');
+					break;
+				case "size" :
+					sb.append(stack.size()).append('\n');
+					break;
+				case "empty" :
+					sb.append(stack.empty() ? 1 : 0).append('\n');
+					break;
+				case "top" :
+					sb.append(stack.empty() ? -1 : stack.peek()).append('\n');
+					break;
+				}
 			}
 		}
 		System.out.println(sb);
