2c8333cc35ea1a95c0859ef48171ee835a2b1873
MiraeChoi <pnenv@naver.com>
2023-01-24 23:59:49 +0900

    add qnas

---
+++ src/k_backjoon/A9012.java
@@ -0,0 +1,24 @@
+package k_backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class A9012 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		while(T-- > 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			String tmp = st.nextToken();
+			if(tmp.equals(")")) {
+				
+			} else {
+				
+			}
+		}
+		System.out.println(sb);
+	}
+}
