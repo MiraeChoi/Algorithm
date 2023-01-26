dc22dd52e187c52d85024123a3365f93384d4044
MiraeChoi <pnenv@naver.com>
2023-01-26 23:59:26 +0900

    edit my answer

--- src/k_backjoon/A4949.java
+++ src/k_backjoon/A4949.java
@@ -3,21 +3,24 @@ package k_backjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 
 class A4949 {
+	private static Stack<Integer> stack = new Stack<>();
 	//Æ²·È½À´Ï´Ù
 	private static boolean check(String str) {
-		int bCnt = 0, sCnt = 0;
 		for(int i = 0; i < str.length(); i++) {
-			if(str.charAt(i) == '(') bCnt++;
-			else if(str.charAt(i) == ')') bCnt--;
-			else if(str.charAt(i) == '[') sCnt++;
-			else if(str.charAt(i) == ']') sCnt--;
-			//Help( I[m being held prisoner in a fortune cookie factory)].
-			if(bCnt < 0 || sCnt < 0) return false;
+			if(str.charAt(i) == '(') stack.push(1);
+			else if(str.charAt(i) == '[') stack.push(2);
+			else if(str.charAt(i) == ')') {
+				if(stack.isEmpty() || stack.pop() != 1) return false;
+			}
+			else if(str.charAt(i) == ']') {
+				if(stack.isEmpty() || stack.pop() != 2) return false;
+			}
 		}
-		if(bCnt != 0 || sCnt != 0) return false;
-		return true;
+		if(!stack.isEmpty()) return false;
+		else return true;
 	}
 	
 	public static void main(String[] args) throws IOException {
