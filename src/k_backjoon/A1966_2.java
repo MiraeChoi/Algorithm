045e312f96a523c32ea8c515d355bb6111df4c6d
MiraeChoi <pnenv@naver.com>
2023-02-11 23:59:46 +0900

    edit my answer

--- src/k_backjoon/A1966_2.java
+++ src/k_backjoon/A1966_2.java
@@ -18,26 +18,27 @@ class A1966_2 {
 			int N = Integer.parseInt(st.nextToken()); //문서의 개수
 			int M = Integer.parseInt(st.nextToken()); //몇 번째로 인쇄되는지
 			st = new StringTokenizer(br.readLine());
-			Queue<Integer> Q = new LinkedList<>();
-			for(int i = 0; i < N; i++) Q.offer(Integer.parseInt(st.nextToken()));
+			Queue<int[]> Q = new LinkedList<>();
+			
+			for(int i = 0; i < N; i++) Q.offer(new int[] {i, Integer.parseInt(st.nextToken())});
 			sb.append("T : ").append(T).append('\n');
 			if(N == 1) sb.append(1).append('\n');
 			else {
-				int cur = Q.poll();
-				int rank = 0;
-				boolean check = false;
-				for(int i = cur + 1; i < N; i++) {
-					rank++;
-					if(Q.contains(i)) {
-						check = Q.offer(cur);
-						System.out.println("Q : " + Q);
-					}
-					if(rank == M) {
-						sb.append(rank).append('\n');
-						continue;
-					}
-				}
-				if(!check) sb.append(rank).append('\n');
+//				int cur = Q.poll();
+//				int rank = 0;
+//				boolean check = false;
+//				for(int i = cur + 1; i < N; i++) {
+//					rank++;
+//					if(Q.contains(i)) {
+//						check = Q.offer(cur);
+//						System.out.println("Q : " + Q);
+//					}
+//					if(rank == M) {
+//						sb.append(rank).append('\n');
+//						continue;
+//					}
+//				}
+//				if(!check) sb.append(rank).append('\n');
 			}
 		}
 		System.out.println(sb);
