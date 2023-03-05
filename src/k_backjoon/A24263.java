fcda95dd8ebf4947840235e2dd3c16be63745112
MiraeChoi <pnenv@naver.com>
2023-03-05 22:44:23 +0900

    add qnas

--- src/k_backjoon/A24263.java
+++ src/k_backjoon/A24263.java
@@ -4,11 +4,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-class A24262 {
+class A24263 {
 	public static void main(String[] args) throws IOException {
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		int n = Integer.parseInt(br.readLine());
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		System.out.println(n);
 		System.out.println(1);
-		System.out.println(0);
 	}
 }
