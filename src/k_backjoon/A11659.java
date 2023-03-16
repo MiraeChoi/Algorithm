1b440c4f48d24e74f1384b916f63e1eef75d7f2a
MiraeChoi <pnenv@naver.com>
2023-03-16 23:59:06 +0900

    add qnas

---
+++ src/k_backjoon/A11659.java
@@ -0,0 +1,31 @@
+package k_backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class A11659 {
+	//시간 초과
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[] arr = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
+		StringBuilder sb = new StringBuilder();
+		while(M-- > 0) {
+			st = new StringTokenizer(br.readLine());
+			int i = Integer.parseInt(st.nextToken()) - 1;
+			int j = Integer.parseInt(st.nextToken()) - 1;
+			int cnt = 0;
+			for(int k = i; k <= j; k++) {
+				cnt += arr[k];
+			}
+			sb.append(cnt).append('\n');
+		}
+		System.out.println(sb);
+	}
+}
