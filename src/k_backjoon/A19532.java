33b79fa8a6b2ea5353f549022f38ce39325e713c
MiraeChoi <pnenv@naver.com>
2023-03-24 17:24:30 +0900

    add a new class

---
+++ src/k_backjoon/A19532.java
@@ -0,0 +1,19 @@
+package k_backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class A19532 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int a = Integer.parseInt(st.nextToken());	int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());	int d = Integer.parseInt(st.nextToken());
+		int e = Integer.parseInt(st.nextToken());	int f = Integer.parseInt(st.nextToken());
+		int answer = 0;
+		
+		System.out.println(answer);
+	}
+}
