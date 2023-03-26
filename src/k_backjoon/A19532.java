8aaff4d633744d69c36ad31a09b0b64de605c5ee
MiraeChoi <pnenv@naver.com>
2023-03-26 23:11:26 +0900

    edit my answer

--- src/k_backjoon/A19532.java
+++ src/k_backjoon/A19532.java
@@ -12,8 +12,39 @@ class A19532 {
 		int a = Integer.parseInt(st.nextToken());	int b = Integer.parseInt(st.nextToken());
 		int c = Integer.parseInt(st.nextToken());	int d = Integer.parseInt(st.nextToken());
 		int e = Integer.parseInt(st.nextToken());	int f = Integer.parseInt(st.nextToken());
-		int answer = 0;
-		
-		System.out.println(answer);
+		int x = 0, y = 0;
+		if(a * d >= b * e) {
+			
+		} else {
+			
+		}
+//		if(Math.abs(a) == Math.abs(d)) {
+//			System.out.println("a");
+//			if(a == d) {
+////				a = a - d;
+//				b = b - e;
+//				c = c - f;
+//			} else {
+////				a = a + d;
+//				b = b + e;
+//				c = c + f;
+//			}
+//			y = b / c;
+//			x = (f - e) / d;
+//		} else if(Math.abs(b) == Math.abs(e)) {
+//			System.out.println("b");
+//			if(b == e) {
+//				a = a - d;
+//				c = c - f;
+//			} else {
+//				a = a + d;
+//				c = c + f;
+//			}
+//			x = a / c;
+//			y = (f - d) / e;
+//		} else {
+//			System.out.println("±× ¿Ü");
+//		}
+		System.out.println(x + " " + y);
 	}
 }
