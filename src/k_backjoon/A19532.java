354f75c61cc36b7d2268bb1796957d961ba605c7
MiraeChoi <pnenv@naver.com>
2023-03-27 23:58:21 +0900

    edit my answer

--- src/k_backjoon/A19532.java
+++ src/k_backjoon/A19532.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 class A19532 {
+	//런타임 에러 /by zero
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
@@ -13,38 +14,20 @@ class A19532 {
 		int c = Integer.parseInt(st.nextToken());	int d = Integer.parseInt(st.nextToken());
 		int e = Integer.parseInt(st.nextToken());	int f = Integer.parseInt(st.nextToken());
 		int x = 0, y = 0;
+		int a1 = a, b1 = b, c1 = c, d1 = d, e1 = e, f1 = f;
 		if(a * d >= b * e) {
-			
+			int n1 = e;
+			int n2 = b;
+			System.out.println("n1 n2 : " + n1 + " / " + n2);
+			a1 *= n1; b1 *= n1; c1 *= n1;
+			d1 *= n2; e1 *= n2; f1 *= n2;
+			if(b1 == e1) { a1 -= d1; b1 -= e1; c1 -= f1; }
+			else		 { a1 += d1; b1 += e1; c1 += f1; }
+			x = c1 / a1;
+			y = (c - a * x) / b;
 		} else {
 			
 		}
-//		if(Math.abs(a) == Math.abs(d)) {
-//			System.out.println("a");
-//			if(a == d) {
-////				a = a - d;
-//				b = b - e;
-//				c = c - f;
-//			} else {
-////				a = a + d;
-//				b = b + e;
-//				c = c + f;
-//			}
-//			y = b / c;
-//			x = (f - e) / d;
-//		} else if(Math.abs(b) == Math.abs(e)) {
-//			System.out.println("b");
-//			if(b == e) {
-//				a = a - d;
-//				c = c - f;
-//			} else {
-//				a = a + d;
-//				c = c + f;
-//			}
-//			x = a / c;
-//			y = (f - d) / e;
-//		} else {
-//			System.out.println("그 외");
-//		}
 		System.out.println(x + " " + y);
 	}
 }
