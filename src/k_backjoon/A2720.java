1d9a88fa6d153f12ca99acd1ebb08e3c1c5b37cf
MiraeChoi <pnenv@naver.com>
2023-03-31 00:51:38 +0900

    add qnas

---
+++ src/k_backjoon/A2720.java
@@ -0,0 +1,20 @@
+package k_backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class A2720 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		int[] money = {25, 10, 5, 1};
+		StringBuilder sb = new StringBuilder();
+		while(T-- > 0) {
+			int[] answer = new int[4];
+			int C = Integer.parseInt(br.readLine()) * 100;
+			
+		}
+		System.out.println(sb);
+	}
+}
