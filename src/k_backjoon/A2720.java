167f849c7da6741759e187a73f34be72bc20e5c3
MiraeChoi <pnenv@naver.com>
2023-04-01 23:58:57 +0900

    add qnas

--- src/k_backjoon/A2720.java
+++ src/k_backjoon/A2720.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 class A2720 {
+	//맞았습니다!!
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
@@ -12,8 +13,15 @@ class A2720 {
 		StringBuilder sb = new StringBuilder();
 		while(T-- > 0) {
 			int[] answer = new int[4];
-			int C = Integer.parseInt(br.readLine()) * 100;
-			
+			int C = Integer.parseInt(br.readLine());
+			for(int i = 0; i < money.length; i++) {
+				while(money[i] <= C) {
+					C -= money[i];
+					answer[i]++;
+				}
+			}
+			for(int i = 0; i < answer.length; i++) sb.append(answer[i]).append(' ');
+			sb.append('\n');
 		}
 		System.out.println(sb);
 	}
