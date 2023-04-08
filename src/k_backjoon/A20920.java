8c405aa4431e0b551e806664a8985d389b114a3d
MiraeChoi <pnenv@naver.com>
2023-04-09 01:14:33 +0900

    add qnas

---
+++ src/k_backjoon/A20920.java
@@ -0,0 +1,43 @@
+package k_backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.StringTokenizer;
+
+class A20920 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		Map<String, Integer> map = new HashMap<>();
+		while(N-- > 0) {
+			String word = br.readLine();
+			if(word.length() < M) continue;
+			map.put(word, map.get(word) == null ? 1 : map.get(word) + 1);
+		}
+		List<Map.Entry<String, Integer>> list = new LinkedList<>(map.entrySet());
+		list.sort(new Comparator<Map.Entry<String, Integer>>() {
+			//1 자주 나오는 단어일수록 앞에 배치한다.
+			//2 해당 단어의 길이가 길수록 앞에 배치한다.
+			//3 알파벳 사전 순으로 앞에 있는 단어일수록 앞에 배치한다.
+			@Override
+			public int compare(Entry<String, Integer> o1, Entry<String, Integer> o2) {
+//				if(o1.getValue() == o2.getValue()) return o1.getKey() - o2.getKey();
+				return o2.getValue() - o1.getValue();
+			}
+		});
+		StringBuilder sb = new StringBuilder();
+		for(Map.Entry<String, Integer> entry : list) {
+			sb.append(entry.getKey()).append(" : ").append(entry.getValue()).append('\n');
+		}
+		System.out.println(sb);
+	}
+}
--- src/k_backjoon/A26069.java
+++ src/k_backjoon/A26069.java
@@ -3,13 +3,24 @@ package k_backjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
 
-public class A26069 {
+class A26069 {
+	//맞았습니다!!
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
+		Set<String> arr = new HashSet<>();
+		arr.add("ChongChong");
 		while(N-- > 0) {
-			
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			String p1 = st.nextToken();
+			String p2 = st.nextToken();
+			if(arr.contains(p1)) arr.add(p2);
+			else if(arr.contains(p2)) arr.add(p1);
 		}
+		System.out.println(arr.size());
 	}
 }
