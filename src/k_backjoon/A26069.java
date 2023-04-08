606ee826d88eeeea6a32aa4cacc261f274ac273d
MiraeChoi <pnenv@naver.com>
2023-04-08 23:59:35 +0900

    add a new class

---
+++ src/k_backjoon/A26069.java
@@ -0,0 +1,15 @@
+package k_backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class A26069 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		while(N-- > 0) {
+			
+		}
+	}
+}
