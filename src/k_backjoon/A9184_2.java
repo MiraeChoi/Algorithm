c26bd262032f96eeeb9b8098b3a392cb97421f87
MiraeChoi <pnenv@naver.com>
2023-04-15 01:15:14 +0900

    edit my answer

--- src/k_backjoon/A9184_2.java
+++ src/k_backjoon/A9184_2.java
@@ -6,14 +6,15 @@ import java.io.BufferedReader;
 import java.io.IOException;
 
 class A9184_2 {
-	private static int[][][] dp;
+	//맞았습니다!!
+	private static int[][][] dp = new int[21][21][21];
 	
 	private static int w(int a, int b, int c) {
+		if(0 <= a && a <= 20 && 0 <= b && b <= 20 && 0 <= c && c <= 20 && dp[a][b][c] != 0) return dp[a][b][c];
 		if(a <= 0 || b <= 0 || c <= 0) return 1;
-		if(dp[a][b][c] != 0) return dp[a][b][c];
 		if(a > 20 || b > 20 || c > 20) return dp[20][20][20] = w(20, 20, 20);
 		if(a < b && b < c) return dp[a][b][c] = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c);
-		return w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1);
+		return dp[a][b][c] = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1);
 	}
 	
 	public static void main(String[] args) throws IOException {
@@ -24,12 +25,19 @@ class A9184_2 {
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			int c = Integer.parseInt(st.nextToken());
-			System.out.println(a + " / " + b + " / " + c);
 			if(a == -1 && b == -1 && c == -1) break;
-			dp = new int[21][21][21];
 			sb.append("w(").append(a).append(", ").append(b).append(", ").append(c).append(") = ")
 				.append(w(a, b, c)).append("\n");
 		}
 		System.out.println(sb);
+//		for(int i = 1; i <= 20; i++) {
+//			for(int j = 1; j <= 20; j++) {
+//				for(int k = 1; k <= 20; k++) {
+//					System.out.print(dp[i][j][k] + " ");
+//				}
+//				System.out.println();
+//			}
+//			System.out.println();
+//		}
 	}
 }
