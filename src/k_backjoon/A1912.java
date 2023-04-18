31825d318805902869a25aba0c033eca35aff8df
MiraeChoi <pnenv@naver.com>
2023-04-18 17:56:12 +0900

    add qnas

---
+++ src/k_backjoon/A1912.java
@@ -0,0 +1,25 @@
+package k_backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class A1912 {
+	//Æ²·È½À´Ï´Ù
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int answer = 0;
+		int cnt = 0;
+		for(int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			if(cnt + arr[i] > cnt && cnt + arr[i] > arr[i]) cnt += arr[i];
+			else cnt = arr[i];
+			answer = Math.max(answer, cnt);
+		}
+		System.out.println(answer);
+	}
+}
