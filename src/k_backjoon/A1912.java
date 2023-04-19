fbb009d969dd132cc1c1d3f7b6f9087e68d9584a
MiraeChoi <pnenv@naver.com>
2023-04-19 23:59:33 +0900

    edit my answer

--- src/k_backjoon/A1912.java
+++ src/k_backjoon/A1912.java
@@ -6,16 +6,21 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 class A1912 {
-	//Æ²·È½À´Ï´Ù
+	//Æ²·È½À´Ï´Ù.
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		int[] arr = new int[n];
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		for(int i = 0; i < n; i++) arr[i] = Integer.parseInt(st.nextToken());
 		int answer = 0;
 		int cnt = 0;
 		for(int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
+			int lt = 0, rt = 0 + i;
+			for(int j = lt; j < n; j++) {
+				cnt += arr[j];
+				lt++; rt++;
+			}
 			if(cnt + arr[i] > cnt && cnt + arr[i] > arr[i]) cnt += arr[i];
 			else cnt = arr[i];
 			answer = Math.max(answer, cnt);
