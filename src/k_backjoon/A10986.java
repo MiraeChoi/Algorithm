068c4037393268e3cdf1eaf3cf1c427edf3cf48e
MiraeChoi <pnenv@naver.com>
2023-04-21 23:59:26 +0900

    edit my answer

--- src/k_backjoon/A10986.java
+++ src/k_backjoon/A10986.java
@@ -6,24 +6,21 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 class A10986 {
+	//맞았습니다!!
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		st = new StringTokenizer(br.readLine());
-		int[] arr = new int[N];
-		int answer = 0;
-		for(int i = 0; i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
+		int[] arr = new int[M];
+		int sum = 0;
 		for(int i = 0; i < N; i++) {
-			//1 2
-			//1 2 3
-			//1 2 3 1 2
-			//2 3 1
-			//3
-			//3 1 2
-			//1 2
+			sum = (sum + Integer.parseInt(st.nextToken())) % M;
+			arr[sum]++;
 		}
+		long answer = arr[0];
+		for(int i = 0; i < M; i++) answer += (long)arr[i] * (arr[i] - 1) / 2;
 		System.out.println(answer);
 	}
 }
