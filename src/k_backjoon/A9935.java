55217ac5c9419715b8992cb554c69da652f0a2db
MiraeChoi <pnenv@naver.com>
2023-04-24 23:59:44 +0900

    add a new class

---
+++ src/k_backjoon/A9935.java
@@ -0,0 +1,14 @@
+package k_backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class A9935 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		String bomb = br.readLine();
+		
+	}
+}
