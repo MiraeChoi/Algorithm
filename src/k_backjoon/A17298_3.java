8ae42af142c1aa1248383b929695c299b46d5558
MiraeChoi <pnenv@naver.com>
2023-05-03 23:58:54 +0900

    add qnas

--- src/k_backjoon/A17298_3.java
+++ src/k_backjoon/A17298_3.java
@@ -3,32 +3,28 @@ package k_backjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
-class A17298_2 {
-	//맞았습니다!!
+class A17298_3 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		Stack<Integer> stack = new Stack<>();
+		int[] stack = new int[N];
 		int[] arr = new int[N];
+		int top = -1;
 		StringBuilder sb = new StringBuilder();
 		for(int i = 0; i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
 		for(int i = 0; i < N; i++) {
-			while(!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
-				System.out.println(arr[stack.peek()] + " / " + arr[i]);
-				arr[stack.pop()] = arr[i];
+			while(top != -1 && arr[stack[top]] < arr[i]) {
+				arr[stack[top]] = arr[i];
+				top--;
 			}
-			stack.push(i);
-			System.out.println("stack : " + stack);
-			System.out.print("arr   : ");
-			for(int j = 0; j < N; j++) System.out.print(arr[j] + " ");
-			System.out.println();
+			top++;
+			stack[top] = i;
 		}
-		while(!stack.isEmpty()) arr[stack.pop()] = -1;
-		for(int i = 0; i < N; i++) sb.append(arr[i]).append(' ');
+		for(int i = top; i >= 0; i--) arr[stack[i]] = -1;
+		for(int x : arr) sb.append(x).append(' ');
 		System.out.print(sb);
 	}
 }
