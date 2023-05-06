f9b00382540dc71d6f0378d76c49ada7228e8856
MiraeChoi <pnenv@naver.com>
2023-05-06 23:58:56 +0900

    add a new class

---
+++ src/k_backjoon/A1725.java
@@ -0,0 +1,19 @@
+package k_backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class A1725 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] square = new int[N];
+		int answer = -1;
+		for(int i = 0; i < N; i++) square[i] = Integer.parseInt(br.readLine());
+		for(int i = 0; i < N; i++) {
+			
+		}
+		System.out.println(answer);
+	}
+}
