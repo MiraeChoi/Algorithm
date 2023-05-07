2b4809ec6ea2622cf9008f362a2115076aa87a0c
MiraeChoi <pnenv@naver.com>
2023-05-07 23:56:51 +0900

    edit my answer

--- src/k_backjoon/A1725.java
+++ src/k_backjoon/A1725.java
@@ -9,10 +9,20 @@ class A1725 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		int[] square = new int[N];
-		int answer = -1;
-		for(int i = 0; i < N; i++) square[i] = Integer.parseInt(br.readLine());
+		int min = Integer.MAX_VALUE;
 		for(int i = 0; i < N; i++) {
-			
+			square[i] = Integer.parseInt(br.readLine());
+			min = Math.min(min, square[i]);
+		}
+		int answer = min * N;
+		int max = square[0], cnt = 1;
+		for(int i = 1; i < N; i++) {
+			if(square[i] > max) cnt++;
+			else cnt = 1;
+			max = Math.min(max, square[i]);
+			int area = max * cnt;
+			answer = Math.max(answer, area);
+			System.out.println("answer : " + answer);
 		}
 		System.out.println(answer);
 	}
