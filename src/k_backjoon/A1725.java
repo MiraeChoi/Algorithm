3f5fae19f2c977b0a8c7709e1e8adc8f1cb5b337
MiraeChoi <pnenv@naver.com>
2023-05-08 00:31:13 +0900

    edit my answer

--- src/k_backjoon/A1725.java
+++ src/k_backjoon/A1725.java
@@ -3,27 +3,29 @@ package k_backjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 
 class A1725 {
-	public static void main(String[] args) throws IOException {
+	//맞았습니다!!
+	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		int[] square = new int[N];
-		int min = Integer.MAX_VALUE;
-		for(int i = 0; i < N; i++) {
-			square[i] = Integer.parseInt(br.readLine());
-			min = Math.min(min, square[i]);
+		int n =  Integer.parseInt(br.readLine());
+		int[] square = new int[n + 2];
+		for(int i = 1; i < n + 1; i++) square[i] = Integer.parseInt(br.readLine());
+		Stack<Integer> stack = new Stack<>();
+		stack.push(0);
+		int answer = 0;
+		for(int i = 1; i <= n + 1; i++) {
+			while(!stack.isEmpty()) {
+				int top = stack.peek();
+				System.out.println("top : " + top);
+				if(square[top] <= square[i]) break;
+				stack.pop();
+				answer = Math.max(answer, square[top] * (i - stack.peek() - 1));
+			}
+			stack.push(i);
+			System.out.println(stack);
 		}
-		int answer = min * N;
-		int max = square[0], cnt = 1;
-		for(int i = 1; i < N; i++) {
-			if(square[i] > max) cnt++;
-			else cnt = 1;
-			max = Math.min(max, square[i]);
-			int area = max * cnt;
-			answer = Math.max(answer, area);
-			System.out.println("answer : " + answer);
-		}
-		System.out.println(answer);
+		System.out.print(answer);
 	}
 }
