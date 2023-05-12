a87464ec245ab178bd04e235c0f7f0bee3613ce5
MiraeChoi <pnenv@naver.com>
2023-05-13 00:54:59 +0900

    edit my answer

--- src/k_backjoon/A24479.java
+++ src/k_backjoon/A24479.java
@@ -5,10 +5,30 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class A24479 {
+class A24479 {
+	private static int[][] arr;
+	private static boolean[] visited;
+	
+	private static void DFS(int V, int E, int R) {
+		visited[R] = true;
+		
+	}
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int R = Integer.parseInt(st.nextToken());
+		arr = new int[M][2];
+		visited = new boolean[N];
+		for(int i = 0; i < arr.length; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < arr[i].length; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		DFS(N, M, R);
 		
 	}
 }
