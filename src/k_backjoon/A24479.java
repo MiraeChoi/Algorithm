e3d73c36d12ff847b98fcf6da6a793058a3df23d
MiraeChoi <pnenv@naver.com>
2023-05-14 23:59:47 +0900

    edit my answer

--- src/k_backjoon/A24479.java
+++ src/k_backjoon/A24479.java
@@ -3,15 +3,16 @@ package k_backjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 class A24479 {
-	private static int[][] arr;
+	private static ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
 	private static boolean[] visited;
+	private static int cnt;
 	
 	private static void DFS(int V, int E, int R) {
 		visited[R] = true;
-		
 	}
 	
 	public static void main(String[] args) throws IOException {
@@ -20,13 +21,14 @@ class A24479 {
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		int R = Integer.parseInt(st.nextToken());
-		arr = new int[M][2];
-		visited = new boolean[N];
-		for(int i = 0; i < arr.length; i++) {
+		for(int i = 0; i <= N; i++) arr.add(new ArrayList<>());
+		visited = new boolean[N + 1];
+		for(int i = 0; i < arr.size(); i++) {
 			st = new StringTokenizer(br.readLine());
-			for(int j = 0; j < arr[i].length; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
-			}
+			int from = Integer.parseInt(st.nextToken());
+			int to = Integer.parseInt(st.nextToken());
+			arr.get(from).add(to);
+			arr.get(to).add(from);
 		}
 		DFS(N, M, R);
 		
