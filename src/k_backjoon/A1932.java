ead2c7064d83cadd943a5358f60b31fead2bd8bc
MiraeChoi <pnenv@naver.com>
2023-05-22 00:42:04 +0900

    edit my answer

--- src/k_backjoon/A1932.java
+++ src/k_backjoon/A1932.java
@@ -6,17 +6,32 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 class A1932 {
+	//맞았습니다!!
+	private static int[][] arr;
+	private static Integer[][] dy;
+	private static int N;
+	
+	private static int DFS(int i, int j) {
+		if(i == N - 1) return dy[i][j];
+		if(dy[i][j] == null) {
+			dy[i][j] = Math.max(DFS(i + 1, j), DFS(i + 1, j + 1)) + arr[i][j];
+		}
+		return dy[i][j];
+	}
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		int N = Integer.parseInt(br.readLine());
-		int[][] arr = new int[N + 1][N + 1];
-		for(int i = 1; i <= N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j = 1; j <= i; j++) {
+		N = Integer.parseInt(br.readLine());
+		arr = new int[N][N];
+		dy = new Integer[N][N];
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			for(int j = 0; j <= i; j++) {
 				arr[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		
+		for(int i = 0; i < N; i++) dy[N - 1][i] = arr[N - 1][i];
+		System.out.print(DFS(0, 0));
 	}
 }
