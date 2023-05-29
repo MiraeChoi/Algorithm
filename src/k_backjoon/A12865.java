fa47bd95eb78338a469e74c530ffd157cfc89c85
MiraeChoi <pnenv@naver.com>
2023-05-29 23:50:30 +0900

    edit my answer

--- src/k_backjoon/A12865.java
+++ src/k_backjoon/A12865.java
@@ -6,26 +6,31 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 class A12865 {
+	//맞았습니다!!
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
-		int[][] bag = new int[N][2];
-		int[] dy = new int[N];
-		for(int i = 0; i < N; i++) {
+		int[][] bag = new int[N + 1][2];
+		for(int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine());
-			for(int j = 0; j < 2; j++) {
-				bag[i][j] = Integer.parseInt(st.nextToken());
+			bag[i][0] = Integer.parseInt(st.nextToken());
+			bag[i][1] = Integer.parseInt(st.nextToken());
+		}
+		int[][] dy = new int[N + 1][K + 1];
+		for(int i = 1; i <= N; i++) {
+			for(int j = 1; j <= K; j++) {
+				if(bag[i][0] > j) dy[i][j] = dy[i - 1][j];
+				else dy[i][j] = Math.max(dy[i - 1][j], dy[i - 1][j - bag[i][0]] + bag[i][1]);
 			}
 		}
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < 2; j++) {
-				System.out.print(bag[i][j] + " ");
-			}
-			System.out.println();
-		}
-		int answer = 0;
-		System.out.println(answer);
+//		for(int i = 1; i <= N; i++) {
+//			for(int j = 1; j <= K; j++) {
+//				System.out.print(dy[i][j] + " ");
+//			}
+//			System.out.println();
+//		}
+		System.out.println(dy[N][K]);
 	}
 }
