fc74b7dbdca7d48eb9ae7789285db2f53d53bf0d
MiraeChoi <pnenv@naver.com>
2023-06-11 23:39:07 +0900

    edit my answer

--- src/k_backjoon/A11660.java
+++ src/k_backjoon/A11660.java
@@ -6,16 +6,17 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 class A11660 {
+	//맞았습니다!!
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
-		int[][] arr = new int[N][N];
-		for(int i = 0; i < N; i++) {
+		int[][] arr = new int[N + 1][N + 1];
+		for(int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
-			for(int j = 0; j < N; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
+			for(int j = 1; j <= N; j++) {
+				arr[i][j] = arr[i][j - 1] + Integer.parseInt(st.nextToken());
 			}
 		}
 		StringBuilder sb = new StringBuilder();
@@ -26,6 +27,7 @@ class A11660 {
 			int y1 = Integer.parseInt(st.nextToken());
 			int x2 = Integer.parseInt(st.nextToken());
 			int y2 = Integer.parseInt(st.nextToken());
+			for(int i = x1; i <= x2; i++) answer += arr[i][y2] - arr[i][y1 - 1];
 			sb.append(answer).append('\n');
 		}
 		System.out.print(sb);
