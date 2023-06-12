3a8130dfb31acb1734d517301f608a8b5f5d3d34
MiraeChoi <pnenv@naver.com>
2023-06-12 23:53:38 +0900

    add qnas

--- src/k_backjoon/A11660_1.java
+++ src/k_backjoon/A11660_1.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-class A11660 {
+class A11660_1 {
 	//맞았습니다!!
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
